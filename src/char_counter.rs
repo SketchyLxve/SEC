2fd197866cadd4ddbe7590eb042466c435c8d5e1
SketchyLxve <twitchisadeck@gmail.com>
2020-08-04 22:47:28 +0200

    refactor: filter immediately rather than checking in the loop

--- src/char_counter.rs
+++ src/char_counter.rs
@@ -1,19 +1,20 @@
 use std::collections::HashMap;
+use std::time::Instant;
 
 pub fn count(x: &'static str) -> HashMap<char, u32> {
+  let now = Instant::now();
   let mut chars: HashMap<char, u32> = HashMap::new();
+  let captures = x
+    .chars()
+    .filter(|&x| x != ' ' && x != ',')
+    .collect::<Vec<char>>();
 
-  for c in x.chars() {
-    if c == ' ' || c == ',' {
-      continue;
-    }
+  for c in captures {
     *chars.entry(c).or_insert(0) += 1;
   }
 
   let highest = *chars.values().max().unwrap_or(&0);
-
-  chars
-    .into_iter()
-    .filter(|&(_k, v)| v == highest)
-    .collect()
+  let end = now.elapsed();
+  println!("{:?}", end);
+  chars.into_iter().filter(|&(_k, v)| v == highest).collect()
 }
