26aee4927b70cbacf1ab4c1de4cbd0b2f7fdb3f7
SketchyLxve <twitchisadeck@gmail.com>
2020-08-05 00:40:11 +0200

    small update

--- src/insertion_sort.rs
+++ src/insertion_sort.rs
@@ -1,13 +1,16 @@
+use std::time::Instant;
+
 pub fn insertion_sort(ints: &mut [i64]) -> &mut [i64] {
+  let now = Instant::now();
   println!("Initial = {:?}", ints);
-  for i in 0..ints.len() {
-    let mut j = i;
-    while j > 0 && ints[j - 1] > ints[j] {
-      ints.swap(j - 1, j);
-      j -=  1;
+  for mut i in 0..ints.len() {
+    while i > 0 && ints[i - 1] > ints[i] {
+      ints.swap(i - 1, i);
+      i -=  1;
     }
   }
 
   println!("Result = {:?}", ints);
-  return ints;
+  println!("End! {:?}", now.elapsed());
+  ints
 }
--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,6 @@ fn main() {
   let extracted =
     char_counter::count("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam lacinia diam condimentum euismod eleifend. Proin faucibus urna in sagittis pharetra. Integer ut velit et orci rutrum ullamcorper vitae sit amet orci. Maecenas non nisi in magna feugiat accumsan a sed justo. Praesent imperdiet augue velit, eu dapibus lectus luctus a. Ut fringilla quis mi quis ultrices. Proin rhoncus purus ex, euismod bibendum augue viverra venenatis. Maecenas magna neque, tempus nec massa eu, dictum elementum ex. Aliquam ut sapien consequat, viverra ipsum at, tempus ex. Proin non convallis odio. Curabitur tristique augue quam, at maximus turpis posuere et. Maecenas hendrerit tortor quis maximus efficitur. Duis ante tellus, dignissim id aliquet auctor, bibendum ut nisi. Sed velit nisl, consequat eget aliquet sit amet, condimentum a ligula. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.");
 
-  insertion_sort::insertion_sort(&mut [3, 5, 7, 8, 9, 1, 2, 6, 4]);
+  insertion_sort::insertion_sort(&mut [3, 5, 7, 8, 9, 1, 2, 6, 4, 22, 34, 562, 4123, 12, 35, 657]);
   println!("{:?}", extracted);
 }
